f7d254ddb64fd2319a0ee5d5de652df29804021c
unknown <aliakhtar.faisal@gmail.com>
2022-06-21 01:09:32 +0530

    adding react todo app source code

---
+++ src/components/TodoForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    });
+    setInput("");
+  };
+
+  return (
+    <section className="todo-form">
+      <form className="form-group" onSubmit={handleSubmit}>
+        {props.edit ? (
+          <>
+            <input
+              type="text"
+              placeholder="Update your task"
+              value={input}
+              name="text"
+              className="todo-input edit"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button onClick={handleSubmit} className="todo-button edit">
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              placeholder="write your task..."
+              value={input}
+              name="text"
+              className="todo-input"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button onClick={handleSubmit} className="todo-button">
+              Add todo
+            </button>
+          </>
+        )}
+      </form>
+    </section>
+  );
+}
+
+export default TodoForm;
